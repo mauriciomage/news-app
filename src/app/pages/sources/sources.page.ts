575189c360e8401be39e3c825028e48d74bdc37a
Mauricio <mauriciomage@gmail.com>
2023-01-17 10:26:43 +0100

    sources page content

--- src/app/pages/sources/sources.page.ts
+++ src/app/pages/sources/sources.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NewsService } from 'src/app/services/news.service';
 
 @Component({
   selector: 'app-sources',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SourcesPage implements OnInit {
 
-  constructor() { }
+  sources: any;
+  fakeSources = new Array(10);
+
+  constructor(
+    private service: NewsService
+  ) { }
 
   ngOnInit() {
+    this.sources = this.service.getData('sources').subscribe((resp: {}) => {
+      this.sources = resp;
+    });
   }
 
 }
